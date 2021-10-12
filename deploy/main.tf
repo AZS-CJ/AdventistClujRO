919e15cde6e720bef59f9123650384296633f74a
Toma Becea <toma.becea@hanna.ro>
2021-10-12 21:14:22 +0300

    Changed the name of the resource group where the terraform state is staying.

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "common-rg"
+    resource_group_name  = "terraform-state"
     storage_account_name = "adventistclujstorage"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"

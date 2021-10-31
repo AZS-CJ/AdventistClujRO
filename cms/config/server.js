86a02a11e3f794e10d1c11497e2aecd5b21e4c72
Toma Becea <toma.becea@hanna.ro>
2021-10-31 10:56:17 +0200

    Strapi configuration files.
    Initial commit, not yet something final or thoroughly verified.

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ba3bd67b0a78eccfd88bd755d98c4d7f'),
+    },
+  },
+});

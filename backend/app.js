b1e7119fbc1774662c6ca9fca71a9bbbfa4e5337
Betty Melania <melania_betty_95@yahoo.com>
2021-11-30 18:50:55 +0200

    Login with google

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const config = require('./config');
 
-passport.use(new Strategy({
+passport.use(new FacebookStrategy({
         clientID: config.FACEBOOK_CLIENT_ID,
         clientSecret: config.FACEBOOK_CLIENT_SECRET,
-        callbackURL: `${config.FRONTEND_HOST}/facebook/callback`,
+        callbackURL: `http://localhost:3001/auth/facebook/callback`,
         profileFields: ['id', 'displayName', 'email', 'name', 'photos'],
         passReqToCallback: true,
         proxy: true
@@ -16,44 +17,62 @@ passport.use(new Strategy({
     function(req, accessToken, refreshToken, profile, cb) {
         // save the profile on the Database
         // Save the accessToken and refreshToken if you need to call facebook apis later on
-        return cb(null, profile);
-    }));
+        return cb(null, profile, accessToken);
+    }
+));
+
+passport.use(new GoogleStrategy({
+        clientID:     config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: `http://localhost:3001/auth/google/callback`,
+        passReqToCallback   : true,
+        proxy: true
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        //TODO - save token to BE?
+        return done(null, profile, accessToken);
+    }
+));
+
+
 passport.serializeUser(function(user, cb) {
     cb(null, user);
 });
 
-console.log('passport-----------------------------------------')
-
 passport.deserializeUser(function(obj, cb) {
     cb(null, obj);
 });
-
-function logiiin(re) {
-    console.log('logggiiiiiiin ');
-    return passport.authenticate('facebook');
-}
-
-function logiiinCAL(re) {
-    console.log('login CALLLL ');
-    return passport.authenticate('facebook', { failureRedirect: `${config.FRONTEND_HOST}/error`})
-}
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 app.use((req, res, nxt) => {
-  console.log("call made");
   nxt();
 });
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback', logiiinCAL, (req, res) => {
-    console.log('here')
-    res.send(`${config.FRONTEND_HOST}/success`);
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: `${config.FRONTEND_HOST}/?loginFailed=true`}), (req, res) => {
+    if (res.req.user) res.cookie('displayName', res.req.user.displayName)
+    if (res.req.authInfo) res.cookie('token', res.req.authInfo)
+    res.redirect(config.FRONTEND_HOST)
 }) ;
+
+app.get('/auth/google', passport.authenticate('google', { scope: [ 'email', 'profile' ] }))
+app.get('/auth/google/callback', passport.authenticate( 'google', { failureRedirect: `${config.FRONTEND_HOST}/?loginFailed=true` }), (req, res) => {
+    if (res.req.user) res.cookie('displayName', res.req.user.displayName)
+    if (res.req.authInfo) res.cookie('token', res.req.authInfo)
+    res.redirect(config.FRONTEND_HOST)
+}) ;
+
+app.get('/auth/logout', function(req, res){
+    req.logout();
+    res.clearCookie("displayName");
+    res.clearCookie("token");
+    res.redirect('/');
+});
 
 function normalizePort(val) {
   var port = parseInt(val, 10);

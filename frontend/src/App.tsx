24742054f64aace5a9a7ec866817ae7272f4ada7
Andreea <andreeaghiurcuta@gmail.com>
2021-12-09 18:32:29 +0000

    Added router paths.Not working.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { reactPlugin } from './ApplicationInsights'
 import { AppInsightsContext } from '@microsoft/applicationinsights-react-js'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from './pages/NotFound/NotFound'
 
 function App(): JSX.Element | null {
-  // eslint-disable-next-line no-console
   return (
     <AppInsightsContext.Provider value={reactPlugin}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="404Page" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppInsightsContext.Provider>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>This is my Home Page</p>
+    </div>
   )
 }
 

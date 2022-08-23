9aefc7af0d87910f59d6132c3331569c8985fddc
Toma Becea <tomabecea@pm.me>
2022-08-23 08:20:05 +0300

    Switch to cname and custom domain verification id for prod.

--- infra/main.tf
+++ infra/main.tf
@@ -200,7 +200,7 @@ resource "azurerm_dns_zone" "azscj-zone" {
   resource_group_name = azurerm_resource_group.common.name
 }
 
-resource "azurerm_dns_a_record" "adventistclujro-prod" {
+resource "azurerm_dns_cname_record" "adventistclujro-prod" {
   name                = "@"
   zone_name           = azurerm_dns_zone.azscj-zone.name
   resource_group_name = azurerm_resource_group.common.name
@@ -208,8 +208,27 @@ resource "azurerm_dns_a_record" "adventistclujro-prod" {
   record              = azurerm_app_service.webhost["prod"].default_site_hostname
 }
 
-resource "azurerm_dns_a_record" "adventistclujro-test" {
-  name                = "test.adventistcluj.ro"
+resource "azurerm_dns_cname_record" "adventistclujro-prod" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.azscj-zone.name
+  resource_group_name = azurerm_resource_group.common.name
+  ttl                 = 300
+  record              = azurerm_app_service.webhost["prod"].default_site_hostname
+}
+
+resource "azurerm_dns_txt_record" "adventistclujro-prod-verif" {
+  name = "asuid.www"
+  zone_name           = azurerm_dns_zone.azscj-zone.name
+  resource_group_name = azurerm_resource_group.common.name
+  ttl                 = 300
+  
+  record {
+    value = azurerm_app_service.webhost["prod"].custom_domain_verification_id
+  }
+}
+
+resource "azurerm_dns_cname_record" "adventistclujro-test" {
+  name                = "test"
   zone_name           = azurerm_dns_zone.azscj-zone.name
   resource_group_name = azurerm_resource_group.common.name
   ttl                 = 300

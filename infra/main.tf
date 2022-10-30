0b3afb325016e9774b53bd72877b1fa7f9625864
Toma Becea <tomabecea@pm.me>
2022-10-30 09:48:44 +0200

    https only for all app services

--- infra/main.tf
+++ infra/main.tf
@@ -162,6 +162,7 @@ resource "azurerm_app_service" "strapi" {
   location            = azurerm_resource_group.website[each.key].location
   resource_group_name = azurerm_resource_group.website[each.key].name
   app_service_plan_id = azurerm_app_service_plan.web-sites-service-plan.id
+  https_only          = true
 
   identity {
     type = "SystemAssigned"

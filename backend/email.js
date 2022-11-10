b530e36d95a885acf3c3216ca0b8520a07a0ad65
Betty Melania <melania_betty_95@yahoo.com>
2022-11-10 18:29:18 +0100

    Change Email password env name

--- backend/email.js
+++ backend/email.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASS }
+    auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASSWORD }
 });
 
 const handlebarOptions = {

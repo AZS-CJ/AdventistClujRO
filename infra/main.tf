ee14118f558de57a940e60824ef6e857a69b7230
Toma Becea <tomabecea@pm.me>
2022-11-28 12:18:37 +0200

    Hopfullly a unique name now.

--- infra/main.tf
+++ infra/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_application_insights" "AZSClujAppInisghts" {
 
 resource "azurerm_storage_account" "cms-storage" {
   for_each            = var.environments
-  name                = "strapi${each.key}"
+  name                = "strapisa${each.key}"
   resource_group_name = azurerm_resource_group.website[each.key].name
 
   location            = azurerm_resource_group.common.location

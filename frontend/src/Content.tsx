3e17d0f78efac5cc55258a2f28d559321b08470d
Betty Melania <melania_betty_95@yahoo.com>
2024-02-20 12:13:43 +0100

    Add 4 Custom colors (#99)

--- frontend/src/Content.tsx
+++ frontend/src/Content.tsx
@@ -4,16 +4,20 @@ import getBackgroundImages from './api/background'
 import Footer from './components/Footer/Footer'
 import Router from './Router'
 import Sidebar from './components/Sidebar/Sidebar'
+import getStyle from './api/style'
+import { setCSSVariables } from './util/functions'
 
 function Content() {
   const { setBackgroundImages } = useGeneralContext()
   const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    getBackgroundImages().then((bgImages) => {
-      setBackgroundImages(bgImages)
-      setLoading(false)
-    })
+    Promise.all([getBackgroundImages(), getStyle()])
+      .then(([bgImages, style]) => {
+        setBackgroundImages(bgImages)
+        setCSSVariables(style)
+      })
+      .finally(() => setLoading(false))
   }, [])
 
   return loading ? (

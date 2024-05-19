374e3dd8bb433b146381306f944dc03c993ca4e1
Toma Becea <tomabecea@tepidemtech.com>
2024-05-19 08:57:40 +0300

    Moved the upgrade in provisioner

--- infra/main.tf
+++ infra/main.tf
@@ -364,6 +364,10 @@ resource "azurerm_dns_txt_record" "cms-verification" {
 resource "null_resource" "strapi-dns" {
   for_each = var.only_platform_enabled ? var.only_platform : var.sites
   provisioner "local-exec" {
+    command    = "az extension add --name containerapp --upgrade"
+    on_failure = fail
+  }
+  provisioner "local-exec" {
     command    = "az containerapp hostname add --resource-group ${azurerm_resource_group.site-rg[each.value.name].name} --name ${azurerm_container_app.strapi-container[each.value.name].name} --hostname cms.${each.value.domain}"
     on_failure = continue
   }

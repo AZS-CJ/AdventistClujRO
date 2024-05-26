cf7d0b2fda332e6dbb9fcf7a35883217bc240d76
Toma Becea <tomabecea@tepidemtech.com>
2024-05-26 16:15:17 +0300

    validation method txt

--- infra/main.tf
+++ infra/main.tf
@@ -392,7 +392,7 @@ resource "null_resource" "web-hostnames" {
   }
 
   provisioner "local-exec" {
-    command    = "az containerapp hostname bind --resource-group ${azurerm_resource_group.site-rg[each.value.name].name} --name ${azurerm_container_app.web-container[each.value.name].name} --hostname ${each.value.domain} --environment ${azurerm_container_app_environment.platform.id} --validation-method CNAME"
+    command    = "az containerapp hostname bind --resource-group ${azurerm_resource_group.site-rg[each.value.name].name} --name ${azurerm_container_app.web-container[each.value.name].name} --hostname ${each.value.domain} --environment ${azurerm_container_app_environment.platform.id} --validation-method TXT"
     on_failure = fail
   }
 

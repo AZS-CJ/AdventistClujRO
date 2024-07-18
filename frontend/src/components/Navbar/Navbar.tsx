4282d50badee21406d18229b99d5bfac42087d32
Victor Greavu <viktorens10mc@gmail.com>
2024-07-18 14:57:24 +0300

    merging mobile_toggle_color into staging

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useNavigationContext } from '../../contexts/navigation'
-import churchName from '../../assets/church_name_logo.svg'
 import NavbarCollapse from './NavbarCollapse'
 import { getLiveStatus } from '../../api/homePage'
 import { useGeneralContext } from '../../contexts/generalState'

f5ce4d56663d7f45c76c66a6db904fb22ee40ee5
Toma Becea <tomabecea@tepidemtech.com>
2024-08-12 17:55:34 +0300

    no user and pass

--- infra/main.tf
+++ infra/main.tf
@@ -182,8 +182,6 @@ resource "azurerm_linux_web_app" "linux-web-app-strapi" {
 
     application_stack {
       docker_image_name = "${azurerm_container_registry.acr.login_server}/azsstrapi:latest"
-      docker_registry_username = azurerm_container_registry.acr.admin_username
-      docker_registry_password = azurerm_container_registry.acr.admin_password
       docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
     }
   }

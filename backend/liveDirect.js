5caebd12daab9dcdf65990f79bcdba087e5dca36
Toma Becea <tomabecea@tepidemtech.com>
2024-12-08 21:49:43 +0200

    WIP - backend working

--- backend/liveDirect.js
+++ backend/liveDirect.js
@@ -1,5 +1,4 @@
 const undici = require('undici');
-const htmlParser = require('node-html-parser');
 
 // Keep the value in a cache object, so that we don't ask Youtube
 // each time an api call comes in
@@ -14,18 +13,43 @@ const cache = {
   intervalForChecking: 6000 // ms
 };
 
+const API_KEY = process.env.YT_API_KEY;
+
+
 // The unique promise is needed so that concurrent api calls
 // do no trigger multiple youtube queries. One query is enough.
 var uniquePromise = null;
 
+const getYoutubeIdUsingFriendlyName = async (friendlyName) => {
+  try {
+    const url = `https://www.googleapis.com/youtube/v3/channels?part=snippet&forHandle=${friendlyName}&key=${API_KEY}`;
+    const { statusCode, body } = await undici.request(url);
+
+    if (statusCode !== 200) {
+      console.error(`API request failed with status: ${statusCode}`);
+      return null;
+    }
+
+    const data = await body.json();
+
+    if (data.items && data.items.length > 0 && data.items[0].id) {
+      return data.items[0].id;
+    }
+
+    return null;
+  } catch (error) {
+    console.error('Error getting channel ID:', error);
+    return null;
+  }
+}
+
 // The 'magic' query happens here where we get the youtube page
 // as plain html. If the live is on then the canonical link is different.
-const queryYoutube = async (youtubeLiveLink) => {
+const queryYoutube = async (channelFriendlyName) => {
 
-  const API_KEY = '';
-  const CHANNEL_ID = 'AdventistCluj';
+  const channelId = await getYoutubeIdUsingFriendlyName(channelFriendlyName);
 
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&key=${API_KEY}`;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${API_KEY}`;
 
   try {
     const { statusCode, body, trailers, context } = await undici.request(url);
@@ -37,13 +61,10 @@ const queryYoutube = async (youtubeLiveLink) => {
       return;
     }
 
-
     if (data.items && data.items.length > 0) {
       console.log('The channel is live!');
-      data.items.forEach(item => {
-        console.log(`Title: ${item.snippet.title}`);
-        console.log(`Video ID: ${item.id.videoId}`);
-      });
+      cache.isLive = true;
+      cache.url = data.items[0].id.videoId;
     } else {
       console.log('The channel is not live.');
     }
@@ -52,7 +73,7 @@ const queryYoutube = async (youtubeLiveLink) => {
   }
 }
 
-const getLiveStatus = async (youtubeLiveLink) => {
+const getLiveStatus = async (channelFriendlyName) => {
 
   // Take the current date and time and compute the time passed.
   const now = Date.now();
@@ -69,7 +90,7 @@ const getLiveStatus = async (youtubeLiveLink) => {
     } else {
       // Nobody triggered this so we go ahead and do it as part
       // of the current call.
-      uniquePromise = queryYoutube(youtubeLiveLink);
+      uniquePromise = queryYoutube(channelFriendlyName);
       await uniquePromise;
 
       // Make sure we reset this to null so that next calls won't
